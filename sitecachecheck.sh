240d793c424253f91b21e1a2d7f5766a8a22edb3
Jordan Trask <jtrask@gmail.com>
2023-05-24 11:13:25 -0400

    build: Don't know what this is :)

--- sitecachecheck.sh
+++ sitecachecheck.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 URL=$1
 
-usage () { 
+usage () {
 	echo "Usage: sitecachecheck.sh <URL>"
 }
 
 cache_check () {
 	echo "-- Checking $URL cache headers"
 	CURL_OUTPUT=$(curl -s --head $URL)
-	echo $CURL_OUTPUT
-	
+	echo "$CURL_OUTPUT"
+
 }
 
 if [[ -z $URL ]]; then
